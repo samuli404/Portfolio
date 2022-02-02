df5bc0bf926e4baff2c51f91fde870fd29635a65
samuli404 <Samuli.karkkainen10@gmail.com>
2022-02-02 13:04:21 +0200

    Omaportfolio

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ClickCount from './components/clickCount'
 import SomeExampleComponent from './components/example/someExampleComponent'
 import MyFirstComponent from './components/myFirstComponent'
+import styles from './App.module.scss'
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+Link
+} from 'react-router-dom';
 
 type AppProps = {
 
@@ -9,11 +16,25 @@ type AppProps = {
 
 const App: React.FC<AppProps> = () => {
   return (
-    <div>
-      <ClickCount/>
-      <SomeExampleComponent/>
-      <MyFirstComponent/>
-    </div>
+     // Important! add your repositoryname to basename
+     <Router basename='/portfolio'>
+      
+     {/* The navigation bar and other components you want to display on all pages come here */}
+     <div className={styles.navbar}>
+       <Link to='/'>Front page</Link>
+       <Link to='example'>Look my example component</Link>
+     </div>
+
+     <Switch>
+       {/* Changing content comes here */}
+       <Route exact path='/' component={MyFirstComponent} />
+       <Route exact path='/example' component={SomeExampleComponent}/>
+     </Switch>
+
+     {/* The footer and other components you want to display on all pages come here */}
+     <ClickCount />
+
+   </Router>
   )
 }
 
