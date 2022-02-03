44343aefcc20b53b3305a8b55238c5b4c3238436
samuli404 <Samuli.karkkainen10@gmail.com>
2022-02-03 12:10:50 +0200

    Korjauksia

---
+++ src/components/pages/main/project/cv/cv.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type CvProps = {
+
+}
+
+const Cv: React.FC<CvProps> = () => {
+  return (
+    <div>
+      Cv
+    </div>
+  )
+}
+
+export default Cv
---
+++ src/components/pages/main/project/project.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type ProjectProps = {
+
+}
+
+const Project: React.FC<ProjectProps> = () => {
+  return (
+    <div>
+      Project
+    </div>
+  )
+}
+
+export default Project
