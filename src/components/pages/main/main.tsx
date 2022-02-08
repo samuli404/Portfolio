ba2ebeac1803ee6c3ea963bad8fccdcb8fd0f8eb
samuli404 <Samuli.karkkainen10@gmail.com>
2022-02-08 15:03:28 +0200

    tiistainportfoliot

--- src/components/pages/main/main.tsx
+++ src/components/pages/main/main.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styles from './main.module.scss'
+import kuva from '../../media3/Muokattu.jpg'
 
 type MainProps = {
 
@@ -6,7 +8,12 @@ type MainProps = {
 
 const Main: React.FC<MainProps> = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.main1}></div>
+      <div className={styles.main2}>
+        <img className={styles.kuva} src={kuva} alt="ei toimi"/>
+      </div>
+      
       Main
     </div>
   )
