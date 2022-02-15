8af830478b22720f0a4b25aea29d6ba19395a287
samuli404 <Samuli.karkkainen10@gmail.com>
2022-02-15 15:39:56 +0200

    Tiistain portfoliota eteenpain

--- src/components/pages/main2/main.tsx
+++ src/components/pages/main2/main.tsx
@@ -10,13 +10,18 @@ const Main: React.FC<MainProps> = () => {
     <div className={styles.container}>
       <div className={styles.contentRow}>
         <div className={styles.contentLeft}>
-            <h1>Moi oon Samppa =D</h1>
+            <h1>Hei olen Samuli</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum deserunt voluptatem vel ad sequi, similique excepturi dolor veniam porro cumque modi nam a nemo placeat officiis, assumenda, praesentium ea tenetur repellendus culpa amet eius maiores. Esse accusamus perspiciatis architecto porro fugit laudantium optio debitis quis mollitia, repellat vero dolorum nihil.</p>
+            <h2>My first game</h2>
         </div>
         <div className={styles.contentRight}>
             <img src={kuva} alt="Kuva on rikki D:" />
         </div>
       </div>
+      <div className={styles.contentTop}>
+          
+        </div>
+      
 
       
     </div>
