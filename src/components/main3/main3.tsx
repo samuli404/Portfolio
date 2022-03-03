87d5b4f5732ac6f954cfa1f86d4fadcc10d13d5d
Samuli Kärkkäinen <samulikarkkainen@Ohjelmointi-14-MacBook-Pro.local>
2022-03-03 14:19:17 +0200

    torstain tehtavat

--- src/components/main3/main3.tsx
+++ src/components/main3/main3.tsx
@@ -11,7 +11,7 @@ const Example3: React.FC = () => {
                 <div className={styles.split50}>
                     <div className={styles.flexLeft}>
                         <h1 >Hei olen Samuli</h1>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dolor, omnis exercitationem adipisci fugiat possimus earum laborum sequi expedita autem sint delectus soluta illum labore quam. Ex saepe quod dolorem eveniet eum itaque, obcaecati qui quisquam quasi sequi nisi ipsa hic at reiciendis quam voluptates doloribus quos perspiciatis corrupti. Ea?</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dolor, omnis </p>
                     </div>
                     <div className={styles.flexRight}>
                         <img src={kuva} alt="gg" />
@@ -21,12 +21,13 @@ const Example3: React.FC = () => {
 
             <div className={styles.flexRow}>
                 <div className={styles.projektiKuvat}>
-                    <img className={styles.imgItem} src={pelikuva} alt="EI toimi!"/>
-                    
+
+                    <div className={styles.unityKuva}></div>
+                    <div className={styles.unityKuva2}></div>
+                    <div className={styles.unityKuva2}></div>
+                    <div className={styles.unityKuva3}></div>
+
                     </div>
-                    <div className={styles.imgItem}></div>
-                    <div className={styles.imgItem}></div>
-                    <div className={styles.imgItem}></div>
                 </div>
             </div>
 
