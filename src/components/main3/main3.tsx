ae8e813c646e5ab4b7bf6cbcf709231749a0ac90
samuli404 <Samuli.karkkainen10@gmail.com>
2022-03-08 15:56:34 +0200

    tiistain jutut

--- src/components/main3/main3.tsx
+++ src/components/main3/main3.tsx
@@ -10,8 +10,8 @@ const Example3: React.FC = () => {
             <div className={`${styles.flexRow} ${styles.first}`}>
                 <div className={styles.split50}>
                     <div className={styles.flexLeft}>
-                        <h1 >Hei olen Samuli</h1>
-                        <p>I'm an software developer student studying in Myyrmäen varia for 3:thrd year and this is my portfolio. </p>
+                        <p >I'm an software developer student studying in Myyrmäen varia for 3:thrd year and this is my portfolio.</p>
+                        
                     </div>
                     <div className={styles.flexRight}>
                         <img src={kuva} alt="gg" />
@@ -20,16 +20,45 @@ const Example3: React.FC = () => {
             </div>
 
             <div className={styles.flexRow}>
-                <div className={styles.projektiKuvat}>
+                <div className={styles.splitpictures}>
+                    <div className={styles.flexup}>
+                        <div className={styles.projektiKuvat}>
 
-                    <div className={styles.unityKuva}></div>
-                    <div className={styles.unityKuva2}></div>
-                    <div className={styles.unityKuva3}></div>
-                    <div className={styles.unityKuva4}></div>
+                            <div className={styles.projectCard}>
+                                <div className={styles.unityKuva}></div>
+                                <p>This is my first game</p>
+                            </div>
+                                
+                            
+                            <div className={styles.projectCard}>
+                                <div className={styles.unityKuva2}></div>
+                                <p>This is my first game</p>
+                            </div>
+                            
+                            <div className={styles.projectCard}>
+                                <div className={styles.unityKuva3}></div>
+                                <p>This is my first game</p>
+                            </div>
+
+                            <div className={styles.projectCard}>
+                                <div className={styles.unityKuva4}></div>
+                                <p>This is my first game</p>
+                            </div>
+                            
+                            </div>
+                        
+                    </div>
+                    <div className={styles.flexdown}>
+                        
+                        <p>There are 2 levels on it</p>
+                        <p>there is even menus with volume sliders on it</p>
+                        <p>and a pause menu</p>
 
                     </div>
                 </div>
+                <div className={styles.information}></div>
             </div>
+        </div>
 
         
     )
