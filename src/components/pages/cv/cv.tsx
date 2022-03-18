7f80873df0134d78d41015a4ec7c3a007ca1800d
samuli404 <Samuli.karkkainen10@gmail.com>
2022-03-18 15:18:41 +0200

    Viimeinen pushhi

--- src/components/pages/cv/cv.tsx
+++ src/components/pages/cv/cv.tsx
@@ -9,19 +9,24 @@ const Cv: React.FC<CvProps> = () => {
   return (
     <div className={styles.container}>
       <div className={styles.education}>
-        <h6>asd</h6>
+        <h6>Education</h6>
+        <div className={styles.studiesinformation}>
+          <p>• I'm currently in school doing 3thrd year in Myyrmaen varia ammattiopisto for IT and Programming.</p>
+          <p>• My best programming languages are: c#, css, html, javascript, node.</p>
+          <p>• I have also completed Cybersecurity introduction, Cyber security essentials, Packet tracer Get connected, Packet tracer intoduction</p>
+        </div>
       </div>
       <div className={styles.cv}>
       <h1>Contacts</h1>
-      <p>Please email me for: Samuli.karkkainen10@gmail.com </p>
-        <h1>Work experience</h1>
+      <h4>Please email me: Samuli.karkkainen10@gmail.com </h4>
+        <h3>Work experience</h3>
         <h2>Digi- ja väestötieto virasto Helsinki</h2>
         <p>2 weeks of on the job learning,</p>
-        <h3>Jakelujuniorit oy</h3>
+        <h2>Jakelujuniorit oy</h2>
         <p>I was a mailman for about an year</p>
-        <h4>Tikkurilan prisma TET</h4>
+        <h2>Tikkurilan prisma TET</h2>
         <p>I was shelfing for 2 weeks</p>
-        <h5>Current job Transval kilo oy</h5>
+        <h2>Current job Transval kilo oy</h2>
         <p>Doing a part time warehouse job in posti</p>
         <div className={styles.certificates}>
           <p>Aaaaaaaa</p>
--- src/components/pages/project/project.tsx
+++ src/components/pages/project/project.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './project.module.scss'
+import { AiOutlineGithub } from 'react-icons/ai'
+import { SiGmail, SiUnity } from 'react-icons/si'
 
 type ProjectProps = {
 
@@ -13,25 +15,53 @@ const Project: React.FC<ProjectProps> = () => {
           <p>Aaaaaaaaaa</p>
         </div>
         <div className={styles.baseborders2}>
-          <div className={styles.projects}>
-          <a href='https://play.unity.com/mg/other/cargamekeskiviikko'>
-            Car game
-          </a>
-          </div>
-          <div className={styles.projects2}>
-          <p>challenge</p>
-          </div>
-          <div className={styles.projects3}>
-          <p>prototype</p>
-          </div>
+            <div className={styles.anim}>
+              <div className={styles.tooltip} data-tooltip="In the game you need to dodge the boxes. • Game is made with Unity c# click it to play it.">
+                <div className={styles.projects}>
+                  <a href='https://play.unity.com/mg/other/cargamekeskiviikko'>
+                    Car game
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className={styles.anim}>
+              <div className={styles.tooltip} data-tooltip="In the game you need to knock off the enemy balls to survive • Game is made with Unity c# click it to play it.">
+                <div className={styles.projects2}>
+                <a href='https://play.unity.com/mg/other/prototype4-16'>
+                    Ball game
+                  </a>
+                </div>
+              </div>
+            </div>
           
-          
+          <div className={styles.anim}>
+            <div className={styles.tooltip} data-tooltip="In the game you need to jump over the fences using space. • Game is made with Unity c# click it to play it.">
+              <div className={styles.projects3}>
+                <a href='https://play.unity.com/mg/other/prototype4-15'>
+                    Fence jumping game
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
+            <div className={styles.baseborders3}>
+              <p>Here you can see few of my projects.</p>
+            </div>
+            <div className={styles.informationbox}>
+                    {/* <ul>
+                    <li className='fa fa-github grow'></li>
+                </ul> */}
+                    <a href='https://github.com/samuli404'>
+                        <AiOutlineGithub/>
+                    </a>
+                <a href='https://gmail.com'>
+                    <SiGmail/>
+                </a>
+                    <a href='https://play.unity.com/u/SamuliKarkkainen'>
+                        <SiUnity/>
+                    </a>
+            </div>
         </div>
-        <div className={styles.baseborders3}>
-          <p>Aaaaaaaaaaa</p>
-        </div>
-      </div>
-
     </div>
   )
 }
